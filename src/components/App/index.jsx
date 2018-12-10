cd06099e4f83d6af1dbd1d86f610c8223d0c969e
Agnel Vignesh B (N.ME) <agneb@wipro.com>
2018-12-10 23:34:34 +0530

    BiolerPlate

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 class App extends Component {
   render() {
@@ -17,7 +19,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React!
           </a>
         </header>
       </div>
